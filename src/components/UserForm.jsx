3aa7142716b4b48b1d749bde8f27c59e0d7b0f0d
cfakhry <132930773+cfakhry@users.noreply.github.com>
2026-09-08 10:29:25 +0300

    bootstrap

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -2,21 +2,45 @@ import { useState } from "react";
 
 const UserForm = () => {
 
-    const [firstName, setFirstName] = useState('John');
-    const [lastName, setLastName] = useState('Doe');
-    const [n, setN] = useState(0);
+    const [person, setPerson] = useState({
+        firstName: "John",
+        lastName: "Doe",
+        age: 39
+    });
 
-    const updateFLName = (n1) =>{
-        setN(n + n1);
-        setFirstName("John "+n);
-        setLastName("Doe "+n);
+    const updateFirstName = (fn) => {
+        setPerson({
+            // spread object
+            ...person,
+
+            firstName: fn
+        }
+        )
+    } 
+
+    const updateLastName = (ln) =>{
+        setPerson({
+            ...person,
+            lastName: "test ln"
+        })
     }
+
+    const upadateAge = (a) => {
+        setPerson({
+            ...person,
+            age: a
+        });
+    }
+
+    
 
     return(
         <>
             <h2>User Form</h2>
-            <p>Welcome {firstName}, {lastName}</p>
-            <button onClick={()=>updateFLName(2)}>Update FN LN</button>
+            <p>Welcome {person.firstName}, {person.lastName}, {person.age}</p>
+            <button className="btn btn-success" onClick={()=>updateFirstName("test")}>Update FN LN</button>
+            <button className="btn btn-danger" onClick={()=> updateLastName("ln test")}>Update Last Name</button>
+            <button className="btn btn-sm btn-secondary" onClick={()=> upadateAge(44)}>Update Age</button>
         </>
     )
 }
